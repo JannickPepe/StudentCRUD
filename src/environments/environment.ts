3ac62743fa2f99487539e7e9ba1b58c8265e0338
Jannick <jannickpedersen@live.dk>
2022-03-03 13:53:00 +0100

    demo cloud firestore CRUD

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVgf5zo_H-c_eGJKEhfrgEIjTajQtDCVw",
+  authDomain: "students-b5c72.firebaseapp.com",
+  projectId: "students-b5c72",
+  storageBucket: "students-b5c72.appspot.com",
+  messagingSenderId: "986493656524",
+  appId: "1:986493656524:web:39dda8e4a631d52eb21169",
+  measurementId: "G-3H3VCL7V4T"
+  }
+};
